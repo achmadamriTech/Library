3b57944e05dce4e90d1b39de81206a96d9f12192
unknown <nyamukterbang8@gmail.com>
2021-08-26 18:52:02 +0700

    Create Entity SewaDetail

--- src/main/java/com/example/library/entity/SewaDetail.java
+++ src/main/java/com/example/library/entity/SewaDetail.java
@@ -52,4 +52,100 @@ public class SewaDetail {
     private String EMAIL;
 
 
+    public Long getID() {
+        return this.ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public Date getTGLSEWA() {
+        return this.TGLSEWA;
+    }
+
+    public void setTGLSEWA(Date TGLSEWA) {
+        this.TGLSEWA = TGLSEWA;
+    }
+
+    public int getLAMASEWA() {
+        return this.LAMASEWA;
+    }
+
+    public void setLAMASEWA(int LAMASEWA) {
+        this.LAMASEWA = LAMASEWA;
+    }
+
+    public String getKETERANGAN() {
+        return this.KETERANGAN;
+    }
+
+    public void setKETERANGAN(String KETERANGAN) {
+        this.KETERANGAN = KETERANGAN;
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getJUDUL() {
+        return this.JUDUL;
+    }
+
+    public void setJUDUL(String JUDUL) {
+        this.JUDUL = JUDUL;
+    }
+
+    public String getPENGARANG() {
+        return this.PENGARANG;
+    }
+
+    public void setPENGARANG(String PENGARANG) {
+        this.PENGARANG = PENGARANG;
+    }
+
+    public String getPELANGGANID() {
+        return this.PELANGGANID;
+    }
+
+    public void setPELANGGANID(String PELANGGANID) {
+        this.PELANGGANID = PELANGGANID;
+    }
+
+    public String getKODEPEL() {
+        return this.KODEPEL;
+    }
+
+    public void setKODEPEL(String KODEPEL) {
+        this.KODEPEL = KODEPEL;
+    }
+
+    public String getNAMA() {
+        return this.NAMA;
+    }
+
+    public void setNAMA(String NAMA) {
+        this.NAMA = NAMA;
+    }
+
+    public String getTELP() {
+        return this.TELP;
+    }
+
+    public void setTELP(String TELP) {
+        this.TELP = TELP;
+    }
+
+    public String getEMAIL() {
+        return this.EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+
 }
