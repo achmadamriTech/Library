875923b9745fb21157da61e2d5be48763107365a
unknown <nyamukterbang8@gmail.com>
2021-08-26 19:24:56 +0700

    Create GetAllSewaData di Controller

--- src/main/java/com/example/library/controller/SewaController.java
+++ src/main/java/com/example/library/controller/SewaController.java
@@ -2,6 +2,7 @@ package com.example.library.controller;
 
 import com.example.library.service.SewaService;
 import com.example.library.entity.Sewa;
+import com.example.library.entity.SewaDetail;
 
 import java.util.List;
 
@@ -21,11 +22,19 @@ public class SewaController {
         this.sewaService = sewaService;
     }
 
+    // Get Semua Data Sewa
     @GetMapping("/")
     public List<Sewa> getAllSewa(){
         return sewaService.getAllSewa();
     }
 
+    // Get Semua Data SewaDetail
+    @GetMapping("/detail/")
+    public List<SewaDetail> getAllSewaDetail(){
+        return sewaService.getAllSewaDetail();
+    }
+
+    // Get Data Berdasarkan ID Sewa
     @GetMapping("/{sewaId}")
     public Sewa get(@PathVariable Long sewaId){
         return sewaService.getAllSewaById(sewaId);
